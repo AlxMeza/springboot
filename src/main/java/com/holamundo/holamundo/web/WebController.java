b89993e9877ee52505ae087d5bf8205b0cf4132e
AlxMeza <alexis.meza24421@gmail.com>
2023-04-26 16:58:05 -0600

    create CRUD to modify persons

--- src/main/java/com/holamundo/holamundo/web/WebController.java
+++ src/main/java/com/holamundo/holamundo/web/WebController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 // import com.holamundo.holamundo.dao.InterfacePersonDao;
 import com.holamundo.holamundo.service.PersonServiceImp;
+import com.holamundo.holamundo.domain.Person;
 
 @Controller
 public class WebController {
@@ -20,5 +23,29 @@ public class WebController {
         var personList = personService.list();
         model.addAttribute("personList", personList);
         return "persons";
+    }
+
+    @GetMapping("/addperson")
+    public String addPerson(Person person){
+        return "configperson";
+    }
+
+    @PostMapping("/save")
+    public String savePerson(Person person){
+        personService.save(person);
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editPerson(Person person, Model model){
+        person = personService.find(person);
+        model.addAttribute("person", person);
+        return "configperson";
+    }
+
+    @GetMapping("/delete")
+    public String deletePerson (Person person){
+        personService.delete(person);
+        return "redirect:/persons";
     }
 }
